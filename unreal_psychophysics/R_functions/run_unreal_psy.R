cf6ab3e6d3b09101cc485bce3883641c1649045c
Gadi Drori <gadidrori@gmail.com>
2024-01-25 14:47:36 +0200

    changes

--- unreal_psychophysics/R_functions/run_unreal_psy.R
+++ unreal_psychophysics/R_functions/run_unreal_psy.R
@@ -1,54 +1,56 @@
 #Psychophysics analysis & Unreal input creating script
-run_unreal_psy<-function(study_number=readline("Study number:"),sub_n = readline("Subject number:"),attempt= readline(prompt = "Fitting attempt number:")){
+run_unreal_psy<-function(study=readline("Study folder (unreal_04/unreal_05/fmri) etc..:"),sub_n = readline("Subject number:"),attempt= readline(prompt = "Fitting attempt number:")){
 
-#type run_unreal_psy() in the console
+  library(here)
+  library(stringi)
+  library(rio)
+  
+  #type run_unreal_psy() in the console
+  #enter name of study: fmri
   #enter subject number:001
   #enter attempt number:1
   #check results in results folder
-  #
+  
   #if you need to edit after a repeat, place the new result in the results folder
-  # and type repeat_jnd() in the console
+  #and type repeat_jnd() in the console
   #enter subject number:001
   #enter attempt number:1
-  #check results
-  
-    
-library(stringi)
-#sub_n <- readline("Subject number:");
-#attempt<- readline(prompt = "Fitting attempt number:"); 
-subject_folder<-gsub(" ","",paste("C:\\Users\\User\\OneDrive\\Desktop\\unreal_psychophysics\\unreal_",study_number,"\\sub_",sub_n))
 
-   
-#C:\Users\User\Desktop\unreal_psychophysics\unreal_05\sub_001
 
-setwd("C:\\Users\\User\\OneDrive\\Desktop\\unreal_psychophysics\\rdas")
-load("repeat_jnd.rda")
-load("psy_paths.rda")
-paths<-psy_paths(subject_folder,attempt,sub_n)
-subject_folder<-paths[1]
-psychophysics_folder<-paths[2]
-output_matlab<-paths[3]
-unreal_random_temps<-paths[4]
-results_folder<-paths[5]
-unreal_input_folder<- paths[6]
-rda_folder<-paths[7]
+#define folders path using here()
+sub_folder_name<-gsub(" ","",paste("sub_",sub_n))
+subject_folder<-here("Studies",study,sub_folder_name)
+results_folder<-here(study,sub_folder_name,gsub(" ","",paste("results_sub_",sub_n,'_attempt_',as.character(attempt))))
+unreal_input_folder<- gsub(" ","",paste(results_folder,"/unreal_input_sub_",sub_n,'_attempt_',as.character(attempt)))
 
-setwd(paths[1]) #subject folder path
-try(dir.create(results_folder),silent=TRUE)
-setwd(paths[5]) #create a folder in results_sub.. to keep the input we made for experiment 2
-try(dir.create(unreal_input_folder),silent=TRUE)
-setwd(paths[7])
-load("name_conditions_and_domains.rda")
-load("unreal_psy.rda")
-load("load_thresholds.rda")
-load("repeat_list.rda")
-#load("unreal_glm.rda")
-unreal_psy(paths,attempt,sub_n,study_number)
-load_thresholds(paths)
+results_folder_name<-gsub(" ","",paste("results_sub_",sub_n,'_attempt_',as.character(attempt)))
+input_folder_name<- gsub(" ","",paste("unreal_input_sub_",sub_n,'_attempt_',as.character(attempt)))
+
+#list of folder names for other functions
+folder_names<-list(sub_folder_name,results_folder_name,input_folder_name)
+
+#create subject results folder
+here(study,sub_folder_name,try(dir.create(results_folder),silent=TRUE))
+
+#create subject input folder for next experiment
+here(study,sub_folder_name,results_folder,try(dir.create(unreal_input_folder),silent=TRUE))
+
+
+here("rdas",load("name_conditions_and_domains.rda"))
+here("rdas",load("unreal_psy.rda"))
+here("rdas",load("load_thresholds.rda"))
+here("rdas",load("repeat_list.rda"))
+
+#run the main function
+unreal_psy(folder_names,attempt,sub_n,study)
+
+#place stimuli in empty input file
+load_thresholds(folder_names)
 
 
 #doAgain contains a list of the conditions we need to redo
-setwd(paths[3])
+load(here("rdas","repeat_jnd.rda"))
+here("output_matlab") 
 doAgain_f<-paste0("doAgain_values_sub_",sub_n,".csv")
 if (file.exists(doAgain_f)){
   doAgain<-read.csv(doAgain_f,header=TRUE, stringsAsFactors = TRUE);
